ab3cff364f312b0f4b2df474e5454877bf1b4a45
vnoook <noook@yandex.ru>
2021-04-22 22:40:33 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# TODO
+'''
+сделать прогу которая ходит по папкам и смотрит чтобы в папке не было больше 5 версий заархивированной базы
+и
+проверяет наличие свободного места и пишет письмо, если место кончается
+'''
+
+import os
+import pathlib
+import sys
+
+db_quantity_in_dir = 5
+
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
