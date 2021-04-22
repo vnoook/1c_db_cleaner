36a39cbc658400b72dc555ceb29a30bb3083fae5
vnoook <noook@yandex.ru>
2021-04-22 22:53:21 +0700

    22 04 2021 #1

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 # TODO
 '''
-сделать прогу которая ходит по папкам и смотрит чтобы в папке не было больше 5 версий заархивированной базы
-и
-проверяет наличие свободного места и пишет письмо, если место кончается
+сделать прогу которая ходит по папкам и смотрит чтобы
+1) в папке не было больше 5 версий заархивированной базы
+2) проверяет наличие свободного места и пишет письмо, если место кончается
 '''
 
 import os
 import pathlib
 import sys
 
+# папка для поиска
+dir_for_clean = r'd:\temp\exp1'
+
+# количество баз в папке
 db_quantity_in_dir = 5
 
-
-
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# почта на которую отправится алерт
+email_alert = 'noook@yandex.ru'
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(dir_for_clean)
+
+
+
+
+
+
+
+
+# os.path.dirname(path1)
+# os.path.realpath(path1)
+# os.getcwd().__iter__()
+# os.fspath(path1)
+# os.path.abspath(name_file)
+# os.path.basename(name_file)
+# os.path.normpath(name_file)
+
+
+
+
 
