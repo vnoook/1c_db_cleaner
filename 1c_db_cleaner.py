2bca7d1e2768417fa29c82f079928a3876e8edbb
vnoook <noook@yandex.ru>
2021-06-16 23:39:41 +0700

    v.1.3.0 добавляю сравнение файлов по содержимому #6

--- 1c_db_cleaner.py
+++ 1c_db_cleaner.py
@@ -188,49 +188,42 @@ def del_arc_files(folder_value):
                         print(' '*4, '--- надо подумать')
 
 
-                # TODO
-                # тут нужно вставить блок со сравнением файлов на содержание
-                print()
-                print('*' * 50)
-                print(f'{list_big_files = }')
-                print(list_big_files.sort())
+            # TODO
+            # тут нужно вставить блок со сравнением файлов на содержание
+            print()
+            print('*' * 50)
+            if len(list_big_files) > 1:
+                list_big_files = sorted(list_big_files, key=lambda nud: nud[2])
                 print(*list_big_files, sep='\n')
                 print()
-                print(*list_big_files[::-1], sep='\n')
-                print()
-                if len(list_big_files) > 1:
-                    list_big_files = sorted(list_big_files, key=lambda nud: nud[2])
-                    # print(*list_big_files, sep='\n')
-                    # print()
 
-                    f_date = 0
-                    f_name = 0
-                    f_size = 0
-                    for file in list_big_files:
-                        if list_big_files.index(file) == 0:
-                            f_date = file[0]
-                            f_name = file[1]
-                            f_size = file[2]
-                            print('первый файл', f_date, f_size, f_name)
-                        else:
-                            print()
-                            # print(list_big_files.index(file), ' __________', f_date, f_size, f_name)
-                            if f_size == file[2]:
-                                print('сравниваю файлы')
-                                print('пред', f_date, f_size, f_name)
-                                print('след', file[0], file[2], file[1])
-                                flag_compare = filecmp.cmp(f_name, file[1], shallow=True)
-                                if flag_compare:
-                                    print('удаляю файл', file[1])
-                                else:
-                                    print('оставляю файл', file[1])
+                f_date = 0
+                f_name = 0
+                f_size = 0
+                for file in list_big_files:
+                    if list_big_files.index(file) == 0:
+                        f_date = file[0]
+                        f_name = file[1]
+                        f_size = file[2]
+                        print('первый файл', f_date, f_size, f_name)
+                    else:
+                        print()
+                        # print(list_big_files.index(file), ' __________', f_date, f_size, f_name)
+                        if f_size == file[2]:
+                            print('сравниваю файлы')
+                            print('пред', f_date, f_size, f_name)
+                            print('след', file[0], file[2], file[1])
+                            flag_compare = filecmp.cmp(f_name, file[1], shallow=True)
+                            if flag_compare:
+                                print('удаляю файл', file[1])
                             else:
-                                pass
-                            f_date = file[0]
-                            f_name = file[1]
-                            f_size = file[2]
-                exit()
-
+                                print('оставляю файл', file[1])
+                        else:
+                            pass
+                        f_date = file[0]
+                        f_name = file[1]
+                        f_size = file[2]
+            exit()
 
             # если осталось больше, чем quantity_files_in_dir, то продолжаю их обрабатывать
             if len(list_big_files) > msc.msc_quantity_files_in_dir:
